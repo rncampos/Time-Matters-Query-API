36800d1e2651718a6eb9621647ab1d7c7d292003
JorgeMendesJumia <jorge.mendes@jumia.com>
2020-04-21 21:46:44 +0100

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flasgger import Swagger
 from flasgger.utils import swag_from
 from flask_cors import CORS
-from Time_Matters_Query import ArquivoPT
+from Time_Matters_Query import ArquivoPT, ArquivoPT_url
 
 
 def main():
@@ -55,11 +55,13 @@ app.config['SWAGGER'] = {
 
 Swagger(app)
 
-def handle_time_matters_query(query, max_items=50, newspaper3k=False, domains=[], beginDate='', endDate='', title=False,snippet=True,fullContent=False, link=''):
-
-    APT = ArquivoPT(max_items, newspaper3k=newspaper3k)
-    result = APT.getResult(query,  domains=domains, beginDate=beginDate, endDate=endDate, title=title,snippet=snippet,fullContent=fullContent, link=link)
-
+def handle_time_matters_query(search_type, query, max_items=50, newspaper3k=False, domains=[], beginDate='', endDate='', title=False,snippet=True,fullContent=False, link=''):
+    if search_type == 'arquivo_pt':
+        APT = ArquivoPT(max_items, newspaper3k=newspaper3k)
+        result = APT.getResult(query,  domains=domains, beginDate=beginDate, endDate=endDate, title=title,snippet=snippet,fullContent=fullContent, link=link)
+    else:
+        APT = ArquivoPT_url(max_items, newspaper3k=newspaper3k)
+        result = APT.getResult(query, beginDate=beginDate, endDate=endDate, title=title, fullContent=fullContent)
     model = {
         'Statistical_info': result[0],
         'Result_articles': result[1]
@@ -71,11 +73,48 @@ def handle_time_matters_query(query, max_items=50, newspaper3k=False, domains=[]
 @app.route("/api/v1.0/arquivopt", methods=['POST'])
 @swag_from('Arquivo_pt.yml')
 def Arquivo_pt():
+    search_type ='arquivo_pt'
+    data = request.form
+
+    query = data['query']
+    max_items = data['max_items']
     try:
-        data = request.form
-        print(data['query'])
-    except Exception as e:
-        return jsonify({'message': str(e)})
+        domains = data.get('domains').replace(' ', '').split(',')
+    except:
+        domains = []
+    beginDate = data.get('beginDate')
+    endDate = data.get('endDate')
+    title = data.get('title')
+    snippet = data.get('snippet')
+    fullContent = data.get('fullContent')
+    newspaper3k = data.get('newspaper3k')
+    link=data.get('link')
+    refactor_noneType = lambda s: s or ""
+    result = handle_time_matters_query(search_type, query, max_items, str2bool(newspaper3k), domains, refactor_noneType(beginDate),
+                                       refactor_noneType(endDate), str2bool(title), str2bool(snippet),
+                                       str2bool(fullContent), refactor_noneType(link))
+    return result
+
+@app.route("/api/v1.0/arquivopt_url", methods=['POST'])
+@swag_from('Arquivo_pt_url.yml')
+def Arquivo_pt_url():
+    search_type = 'arquivo_pt_url'
+    data = request.form
+
+    url = data['url']
+    max_items = data['max_items']
+    beginDate = data.get('beginDate')
+    endDate = data.get('endDate')
+    title = data.get('title')
+    fullContent = data.get('fullContent')
+    newspaper3k = data.get('newspaper3k')
+    refactor_noneType = lambda s: s or ""
+    result = handle_time_matters_query(search_type, query=url, max_items=max_items, newspaper3k=str2bool(newspaper3k), beginDate=refactor_noneType(beginDate),
+                                       endDate=refactor_noneType(endDate), title=str2bool(title), fullContent=str2bool(fullContent))
+    return result
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
 
 if __name__ == '__main__':
